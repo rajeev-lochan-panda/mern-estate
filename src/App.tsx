a1438ecb1ac81a1cf7ae6cb714da6a9b467818a6
rajeev-lochan-panda <pandarajeevlochan@gmail.com>
2024-10-28 09:24:24 +0530

    Created Header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Profile from './pages/Profile';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
@@ -16,7 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
