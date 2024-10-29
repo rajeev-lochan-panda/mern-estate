5193f01901416fd16f8c1360156ee63acaf7e2b1
rajeev-lochan-panda <pandarajeevlochan@gmail.com>
2024-10-29 09:30:11 +0530

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB 📦📦📦");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(() => {
-    console.log("Server is running on port 3000!!! 💻💻💻");
-})
+  console.log("Server is running on port 3000 💻💻💻");
+});
