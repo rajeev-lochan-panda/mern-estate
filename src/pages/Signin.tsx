02bbfc28c34757f4491b16cb1d75b6b0ff1e368d
rajeev-lochan-panda <pandarajeevlochan@gmail.com>
2024-10-29 07:28:41 +0530

    added prettier

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Signin() {
-  return (
-    <div>
-      Sign In
-    </div>
-  )
+  return <div>Sign In</div>;
 }
 
-export default Signin
+export default Signin;
