0b37c297b045ac2e909e536acb7da766503833c8
rajeev-lochan-panda <pandarajeevlochan@gmail.com>
2024-11-10 15:52:32 +0530

    google login api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -64,7 +64,7 @@ export const signIn = async (req, res, next) => {
       return res.status(401).send({
         success: false,
         message: "Wrong Credentials",
-      })
+      });
     }
 
     // Create token
@@ -81,5 +81,49 @@ export const signIn = async (req, res, next) => {
       });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...others } = user._doc;
+      res
+        .cookie("token", token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+        .status(200)
+        .json({
+          success: true,
+          message: "User logged in successfully",
+          data: others,
+        });
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...others } = newUser._doc;
+      res
+        .cookie("token", token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 })
+        .status(200)
+        .json({
+          success: true,
+          message: "User logged in successfully",
+          data: others,
+        });
+    }
+  } catch (error) {
+    res.status(500).json({ error: err.message });
   }
 };
